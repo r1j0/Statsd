6e1cd85c853ce4490df8df3f3bcbf1c249f400b0
r1j0 <r1j0@jamd.local>
2012-04-10 22:11:44 +0200

    use simple threads

--- src/com/github/r1j0/statsd/client/StatsdClient.java
+++ src/com/github/r1j0/statsd/client/StatsdClient.java
@@ -12,7 +12,6 @@ import org.apache.mina.filter.codec.textline.TextLineCodecFactory;
 import org.apache.mina.filter.logging.LoggingFilter;
 import org.apache.mina.transport.socket.nio.NioDatagramConnector;
 
-
 public class StatsdClient {
 
 	private static final String HOST = "127.0.0.1";
@@ -20,25 +19,39 @@ public class StatsdClient {
 
 
 	public static void main(String[] args) throws IOException, InterruptedException {
-		IoConnector connector = new NioDatagramConnector();
-		connector.getSessionConfig().setReadBufferSize(2048);
-
-		connector.getFilterChain().addLast("logger", new LoggingFilter());
-		connector.getFilterChain().addLast("codec", new ProtocolCodecFilter(new TextLineCodecFactory(Charset.forName("UTF-8"))));
-
-		connector.setHandler(new StatsdClientHandler("Hello Server.. äöüß"));
-		ConnectFuture future = connector.connect(new InetSocketAddress(HOST, PORT));
-		future.awaitUninterruptibly();
-
-		if (!future.isConnected()) {
-			return;
+		for (int i = 0; i < 100; i++) {
+			new Automatic().run();
 		}
 
-		IoSession session = future.getSession();
-		session.getConfig().setUseReadOperation(true);
-		session.getCloseFuture().awaitUninterruptibly();
+		System.exit(0);
+	}
 
-		System.out.println("After Writing");
-		connector.dispose();
+
+	public static class Automatic extends Thread {
+
+		public Automatic() {
+			super();
+		}
+
+
+		public void run() {
+			IoConnector connector = new NioDatagramConnector();
+			connector.getSessionConfig().setReadBufferSize(2048);
+
+			connector.getFilterChain().addLast("logger", new LoggingFilter());
+			connector.getFilterChain().addLast("codec", new ProtocolCodecFilter(new TextLineCodecFactory(Charset.forName("UTF-8"))));
+
+			connector.setHandler(new StatsdClientHandler(" Hello Server.. äöüß"));
+			ConnectFuture future = connector.connect(new InetSocketAddress(HOST, PORT));
+
+			if (!future.isConnected()) {
+				return;
+			}
+
+			IoSession session = future.getSession();
+			session.getConfig().setUseReadOperation(true);
+			session.close(true);
+			connector.dispose();
+		}
 	}
 }
